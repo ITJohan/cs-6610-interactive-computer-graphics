aa2492488400421fbf6b9911f48bd1b0b2c3e702
itjohan <johanlindkvist89@gmail.com>
2024-01-22 21:43:19 +0100

    project-2: parse vertices

--- project-2/obj-reader.mjs
+++ project-2/obj-reader.mjs
@@ -6,30 +6,88 @@ fileReader.addEventListener('load', (e) => {
   const parsedModel = lines.reduce(
     (result, line) => {
       if (line.includes('v ')) {
+        const parsedLine = line.split(' ');
+
         return {
           ...result,
-          vertices: [...result.vertices, line],
+          vertices: [
+            ...result.vertices,
+            {
+              x: Number(parsedLine[2]),
+              y: Number(parsedLine[3]),
+              z: Number(parsedLine[4]),
+            },
+          ],
         };
       }
 
       if (line.includes('vn ')) {
+        const parsedLine = line.split(' ');
+
         return {
           ...result,
-          vertexNormals: [...result.vertexNormals, line],
+          vertexNormals: [
+            ...result.vertexNormals,
+            {
+              x: Number(parsedLine[1]),
+              y: Number(parsedLine[2]),
+              z: Number(parsedLine[3]),
+            },
+          ],
         };
       }
 
       if (line.includes('vt ')) {
+        const parsedLine = line.split(' ');
+
         return {
           ...result,
-          textureCoordinates: [...result.textureCoordinates, line],
+          textureCoordinates: [
+            ...result.textureCoordinates,
+            {
+              x: Number(parsedLine[1]),
+              y: Number(parsedLine[2]),
+              z: Number(parsedLine[3]),
+            },
+          ],
         };
       }
 
       if (line.includes('f ')) {
+        const parsedLine = line.split(' ');
+
+        const vertex1 = parsedLine[1].split('/');
+        const vertex2 = parsedLine[2].split('/');
+        const vertex3 = parsedLine[3].split('/');
+        const vertex4 = parsedLine[4].split('/');
+
         return {
           ...result,
-          triangles: [...result.triangles, line],
+          triangles: [
+            ...result.triangles,
+            {
+              vertex1: {
+                x: Number(vertex1[0]),
+                y: Number(vertex1[1]),
+                z: Number(vertex1[2]),
+              },
+              vertex2: {
+                x: Number(vertex2[0]),
+                y: Number(vertex2[1]),
+                z: Number(vertex2[2]),
+              },
+              vertex3: {
+                x: Number(vertex3[0]),
+                y: Number(vertex3[1]),
+                z: Number(vertex3[2]),
+              },
+              vertex4: {
+                x: Number(vertex4[0]),
+                y: Number(vertex4[1]),
+                z: Number(vertex4[2]),
+              },
+            },
+          ],
         };
       }
 
