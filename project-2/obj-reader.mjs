d1bc338a704367d13bed82697728c9b5753bb5f6
itjohan <johanlindkvist89@gmail.com>
2024-01-22 21:31:10 +0100

    project-2: parse lines

--- project-2/obj-reader.mjs
+++ project-2/obj-reader.mjs
@@ -1,7 +1,44 @@
 const fileReader = new FileReader();
 fileReader.addEventListener('load', (e) => {
-  const result = e.target.result;
-  console.log(result);
+  const text = e.target.result;
+  const lines = text.split('\r\n');
+
+  const parsedModel = lines.reduce(
+    (result, line) => {
+      if (line.includes('v ')) {
+        return {
+          ...result,
+          vertices: [...result.vertices, line],
+        };
+      }
+
+      if (line.includes('vn ')) {
+        return {
+          ...result,
+          vertexNormals: [...result.vertexNormals, line],
+        };
+      }
+
+      if (line.includes('vt ')) {
+        return {
+          ...result,
+          textureCoordinates: [...result.textureCoordinates, line],
+        };
+      }
+
+      if (line.includes('f ')) {
+        return {
+          ...result,
+          triangles: [...result.triangles, line],
+        };
+      }
+
+      return result;
+    },
+    { vertices: [], vertexNormals: [], textureCoordinates: [], triangles: [] }
+  );
+
+  console.log(parsedModel);
 });
 
 const inputEl = document.querySelector('input');
